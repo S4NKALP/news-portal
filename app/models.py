1cbe0667e5be18313b5ec0ee37a96166c002e4e2
S4NKALP <sankalptharu50028@gmail.com>
2025-02-13 13:09:54 +0545

    Models: update news model

--- app/models.py
+++ app/models.py
@@ -31,6 +31,8 @@ class News(models.Model):
     title = models.CharField(max_length=255)
     img = models.ImageField(upload_to="news")
     description = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return self.title
