5e1b7310da66d4e69bd180e102d8f1c4c23fffd4
S4NKALP <sankalptharu50028@gmail.com>
2025-02-13 13:06:22 +0545

    added models

--- app/models.py
+++ app/models.py
@@ -1,22 +1,55 @@
 from django.db import models
+
+
 class Category(models.Model):
     id = models.BigAutoField(primary_key=True)
     name = models.CharField(max_length=255)
+
     def __str__(self):
         return self.name
+
     class Meta:
-        ordering = ['-id']
-        verbose_name_plural = 'Categories'
+        ordering = ["-id"]
+        verbose_name_plural = "Categories"
+
 
 class SubCagtegory(models.Model):
     id = models.BigAutoField(primary_key=True)
     name = models.CharField(max_length=255)
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
+
     def __str__(self):
         return self.name
+
     class Meta:
-        ordering = ['-id']
-        verbose_name_plural = 'Sub Categories'
+        ordering = ["-id"]
+        verbose_name_plural = "Sub Categories"
+
 
 class News(models.Model):
-    pass
+    id = models.BigAutoField(primary_key=True)
+    title = models.CharField(max_length=255)
+    img = models.ImageField(upload_to="news")
+    description = models.TextField()
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        ordering = ["-id"]
+        verbose_name_plural = "News"
+
+
+class Advertisement(models.Model):
+    id = models.BigAutoField(primary_key=True)
+    is_active = models.BooleanField(default=True)
+    img = models.ImageField(upload_to="advertisement")
+    link = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.id
+
+    class Meta:
+        ordering = ["-id"]
+        verbose_name_plural = "ads"
+
