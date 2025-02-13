de27f94dbc621ced0ad793028b3ec81062cd5ab9
Nur Pratap Karki <nurpratapkarki@gmail.com>
2025-02-13 12:46:04 +0545

    'changes'

--- newsportal/urls.py
+++ newsportal/urls.py
@@ -15,8 +15,17 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from newsportal.settings import MEDIA_ROOT, MEDIA_URL#, STATIC_URL, STATIC_ROOT
+from django.conf.urls.static import static
+
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    # path('', admin.site.urls),
+    path('api/', include('app.urls')),
+    path("ckeditor5/", include('django_ckeditor_5.urls')),
 ]
+
+urlpatterns += static(MEDIA_URL, document_root=MEDIA_ROOT)
+
+urlpatterns.append(path('', admin.site.urls, name='admin'))
