1a2b053ef2013dcf0f1239aa384f292b3dec7827
Nur Pratap Karki <nurpratapkarki@gmail.com>
2025-02-13 12:54:02 +0545

    'models'

--- app/models.py
+++ app/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+class Category(models.Model):
+    id = models.BigAutoField(primary_key=True)
+    name = models.CharField(max_length=255)
+    def __str__(self):
+        return self.name
+    class Meta:
+        ordering = ['-id']
+        verbose_name_plural = 'Categories'
 
-# Create your models here.
+class SubCagtegory(models.Model):
+    id = models.BigAutoField(primary_key=True)
+    name = models.CharField(max_length=255)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    def __str__(self):
+        return self.name
+    class Meta:
+        ordering = ['-id']
+        verbose_name_plural = 'Sub Categories'
