4b510e4a75a3032eca5295c86cd6dc7a3e409434
S4NKALP <sankalptharu50028@gmail.com>
2025-02-13 15:45:45 +0545

    script: updated take commit message from user

--- push.py
+++ push.py
@@ -1,12 +1,14 @@
 import os
 import subprocess
 
-def push_to_github(repo_path, commit_message):
+
+def push_to_github(repo_path):
     os.chdir(repo_path)
+    commit_message = input("Enter your commit message: ")  # Take input from user
     subprocess.run(["git", "add", "."])
     subprocess.run(["git", "commit", "-m", commit_message])
     subprocess.run(["git", "push"])
 
+
 repo_path = r"C:\Users\nurpr\OneDrive\Desktop\Backend\news-portal"
-commit_message = "Automated commit message"
-push_to_github(repo_path, commit_message)
+push_to_github(repo_path)
