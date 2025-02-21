54481c8e0778288a7e6bf4c9a38ba8c315092fa7
S4NKALP <sankalptharu50028@gmail.com>
2025-02-21 13:39:16 +0545

    serializer update

--- app/serializers.py
+++ app/serializers.py
@@ -1,47 +1,62 @@
 from rest_framework import serializers
 from .models import Category, SubCategory, News, NewsImage, Advertisement, Organization
 
+
 class SubCategorySerializer(serializers.ModelSerializer):
     class Meta:
         model = SubCategory
-        fields = ['id', 'name']
+        fields = ["id", "name"]
+
 
 class CategoryListSerializer(serializers.ModelSerializer):
     subcategories = SubCategorySerializer(many=True, read_only=True)
-    
+
     class Meta:
         model = Category
-        fields = ['id', 'name', 'subcategories']
+        fields = ["id", "name", "subcategories"]
+
 
 class CategorySerializer(serializers.ModelSerializer):
     class Meta:
         model = Category
-        fields = ['id', 'name']
+        fields = ["id", "name"]
+
 
 class NewsImageSerializer(serializers.ModelSerializer):
     class Meta:
         model = NewsImage
-        fields = ['id', 'image']
+        fields = ["id", "image"]
+
 
 class NewsSerializer(serializers.ModelSerializer):
     category = CategorySerializer(read_only=True)
     sub_category = SubCategorySerializer(read_only=True)
     images = NewsImageSerializer(many=True, read_only=True)
-    
+
     class Meta:
         model = News
         fields = [
-            'id', 'title', 'description', 'image', 
-            'category', 'sub_category', 'images',
-            'created_at', 'updated_at'
+            "id",
+            "title",
+            "description",
+            "image",
+            "category",
+            "sub_category",
+            "images",
+            "is_feature",
+            "is_video,created_at",
+            "updated_at",
         ]
+
 
 class AdvertisementSerializer(serializers.ModelSerializer):
     class Meta:
         model = Advertisement
-        fields = ['id', 'is_active', 'img', 'link']
+        fields = ["id", "is_active", "img", "link"]
+
 
 class OrganizationSerializer(serializers.ModelSerializer):
     class Meta:
         model = Organization
-        fields = ['id', 'name', 'location', 'email', 'phone_no', 'logo']
+        fields = ["id", "name", "location", "email", "phone_no", "logo"]
+
